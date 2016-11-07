b21c31512c2c31e826cea91055e90cbdc9845b86
shufengw <imgamer@163.com>
2016-11-07 17:19:50 +0800

    1. 添加游戏启动入口 2. 调试接口封装

---
+++ zerogame/Assets/Game/Scripts/Common/SFDebug.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public static class SFDebug
+{
+    static void Log( string p_formatstring, params object[] p_args )
+    {
+        Debug.Log(string.Format(p_formatstring, p_args));
+    }
+
+    static void Warning(string p_formatstring, params object[] p_args)
+    {
+        Debug.LogWarning(string.Format(p_formatstring, p_args));
+    }
+
+    static void Error( string p_formatstring, params object[] p_args )
+    {
+        Debug.LogError(string.Format(p_formatstring, p_args));
+    }
+}
---
+++ zerogame/Assets/Game/Scripts/Main.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+class Main: MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Game awake...");
+    }
+
+    void Start()
+    {
+        Debug.Log("Game start...");
+    }
+}
