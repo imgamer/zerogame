c7ba829d3a06669b606fecef9e2f641bfdcc55cd
shufengw <imgamer@163.com>
2016-11-08 16:43:07 +0800

    1. 调试模块改进 2. Main入口的singleton检查

--- zerogame/Assets/Game/Scripts/Common/SFDebug.cs
+++ zerogame/Assets/Game/Scripts/Common/SFDebug.cs
@@ -6,16 +6,16 @@ public static class SFDebug
 {
     public static void Log( string p_formatstring, params object[] p_args )
     {
-        Debug.Log(string.Format(p_formatstring, p_args));
+        Debug.Log("[DEBUG]:"+string.Format(p_formatstring, p_args));
     }
 
     public static void Warning(string p_formatstring, params object[] p_args)
     {
-        Debug.LogWarning(string.Format(p_formatstring, p_args));
+        Debug.LogWarning("[WARNING]:"+string.Format(p_formatstring, p_args));
     }
 
     public static void Error( string p_formatstring, params object[] p_args )
     {
-        Debug.LogError(string.Format(p_formatstring, p_args));
+        Debug.LogError("[ERROR]:" + string.Format(p_formatstring, p_args));
     }
 }
--- zerogame/Assets/Game/Scripts/Main.cs
+++ zerogame/Assets/Game/Scripts/Main.cs
@@ -2,13 +2,38 @@ using UnityEngine;
 
 class Main: MonoBehaviour
 {
+    private static Main _instance = null;
+
+    public Main Instance
+    {
+        get
+        {
+            return Main._instance;
+        }
+    }
+
     void Awake()
     {
+        CheckSingleton();
         SFDebug.Log("Game awake...");
     }
 
     void Start()
     {
         SFDebug.Log("Game start...");
+    }
+
+    private void CheckSingleton()
+    {
+        if( _instance == null)
+        {
+            _instance = this;
+            SFDebug.Log("Main::CheckSingleton...good..");
+        }
+        else if( _instance != this )
+        {
+            GameObject.Destroy(this);
+            SFDebug.Warning("There is already a instance of Main created( {0} ), now destoy the new one.", _instance.name );
+        }
     }
 }
