a000428c948230f42e8c4e29f957313cfe1ffdd2
shufeng <imgamer@163.com>
2016-11-10 00:52:57 +0800

    add MonoSingletonMgr

---
+++ zerogame/Assets/Game/Scripts/Common/MonoSingletonMgr.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+
+
+public class MonoSingletonMgr
+{
+	static readonly string SINGLETON_NAME = "Singleton";
+
+	private static MonoSingletonMgr _instance;
+	private static GameObject _singleton;
+
+	private MonoSingletonMgr()
+	{
+		SFDebug.Log("MonoSingletonMgr construction...");
+	}
+
+	public static void Create()
+	{
+		if (_instance == null) 
+		{
+			_instance = new MonoSingletonMgr();
+
+			GameObject singleton = GameObject.Find(SINGLETON_NAME);
+			if(singleton == null)
+			{
+				singleton = new GameObject(SINGLETON_NAME);
+				_instance._singleton = singleton;
+				Object.DontDestroyOnLoad(singleton);
+			}
+
+			InitSingleton();
+		}
+	}
+
+	public static MonoSingletonMgr Instance
+	{
+		get
+		{
+			if(_instance == null)
+			{
+				Create();
+			}
+
+			return MonoSingletonMgr._instance;
+		}
+	}
+
+	private void InitSingleton()
+	{
+	}
+}
