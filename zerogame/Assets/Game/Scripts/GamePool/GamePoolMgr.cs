b3619661bb0e44c7d75b14f850ecb76fef8b9485
shufengw <imgamer@163.com>
2016-11-22 18:48:00 +0800

    开始写游戏对象资源池管理模块

---
+++ zerogame/Assets/Game/Scripts/GamePool/GamePoolMgr.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePoolMgr : MonoBehaviour {
+
+    public static GamePoolMgr Instance { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public GameObject Spawn( string p_gameObjectName, PoolName p_poolName, PoolType p_poolType )
+    {
+        return null;
+    }
+
+    public void DeSpawn( GameObject p_gameobject )
+    {
+    }
+}
---
+++ zerogame/Assets/Game/Scripts/GamePool/PoolDefine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PoolName
+{
+    Model
+}
+
+public enum PoolType
+{
+    Default,    // 不进资源池，不创建不回收
+    Once,       // 一次性资源池，只创建不回收
+    Loop        // 循环型资源池，既创建也回收
+}
