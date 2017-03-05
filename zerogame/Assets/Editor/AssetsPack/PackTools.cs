a3c4078deae5c46038f4707c6fe4371ca2ba688f
shufeng <imgamer@163.com>
2017-03-05 09:44:46 +0800

    资源打包入口实现修改为简单工厂模式

--- zerogame/Assets/Editor/AssetsPack/PackTools.cs
+++ zerogame/Assets/Editor/AssetsPack/PackTools.cs
@@ -2,14 +2,46 @@
 using UnityEditor;
 using System.Collections;
 
+
 public class PackTools
 {
     [MenuItem("BundleTools/Resources方式打包(默认)")]
     public static void PackResources()
     {
-        ResourcesPacker resPacker = new ResourcesPacker();
-        PackDirector director = new PackDirector( resPacker );
-        director.Pack();
+		Pack (AssetsType.Resources);
     }
 
+	[MenuItem("BundleTools/Local Bunlde方式打包")]
+	public static void PackLoaclBundle()
+	{
+		Pack (AssetsType.Local);
+	}
+
+	[MenuItem("BundleTools/Network Bunlde方式打包")]
+	public static void PackNetworkBunlde()
+	{
+		Pack (AssetsType.Server);
+	}
+
+	private static Packer CreatePacker(AssetsType p_assetsType)
+	{
+		switch (p_assetsType) 
+		{
+			case AssetsType.Local:
+				Debug.LogError ("cant find AssetsType.Local Packer.");
+				return null;
+			case AssetsType.Resources:
+				return new ResourcesPacker ();
+			default:
+				Debug.LogError (string.Format("cant find AssetsType:{0} packer.", p_assetsType.ToString()));
+				return null;
+		}
+	}
+
+	private static void Pack(AssetsType p_assetsType)
+	{
+		Packer packer = CreatePacker (p_assetsType);
+		PackDirector director = new PackDirector( packer );
+		director.Pack();
+	}
 }
