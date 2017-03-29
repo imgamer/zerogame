682f990b707f37954448e915cb0f25026ef6c36f
shufeng <imgamer@163.com>
2017-03-29 19:53:43 +0800

    增加打包提示

--- zerogame/Assets/Game/GameAssets/Editor/PackTools.cs
+++ zerogame/Assets/Game/GameAssets/Editor/PackTools.cs
@@ -8,13 +8,27 @@ public class PackTools
     [MenuItem("BundleTools/Resources方式打包(默认)")]
     public static void PackResources()
     {
+        if (!EditorUtility.DisplayDialog("Resources方式打包", "是否开始打包", "Yes", "No"))
+        {
+            return;
+        }
+
 		Pack (AssetsType.Resources);
+
+        EditorUtility.DisplayDialog("完成", "资源打包完成，请检查Console窗口是否有错误提示", "OK");
     }
 
 	[MenuItem("BundleTools/Local Bunlde方式打包")]
 	public static void PackLoaclBundle()
 	{
+        if (!EditorUtility.DisplayDialog("本地Bundle打包", "是否开始打包", "Yes", "No"))
+        {
+            return;
+        }
+
 		Pack (AssetsType.Local);
+
+        EditorUtility.DisplayDialog("完成", "资源打包完成，请检查Console窗口是否有错误提示", "OK");
 	}
 
 	[MenuItem("BundleTools/Network Bunlde方式打包")]
